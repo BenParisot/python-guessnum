d7849042adc19412d90ce1cc274020ee29a8db30
Ben Parisot <me@benparisot.com>
2019-08-20 09:48:00 -0700

    add name input and name/guess in response messages

--- program.py
+++ program.py
@@ -9,15 +9,17 @@ the_number = random.randint(0, 100)
 
 guess_int = -1
 
+name = input('What is your name?')
+
 while guess_int != the_number:
     guess_text = input('Guess a number between 0 and 100: ')
     guess_int = int(guess_text)
 
     if guess_int < the_number:
-        print('Your guess is too low')
+        print('Sorry {}, your guess is too low'.format(name))
     elif guess_int > the_number:
-        print('Your guess is too high')
+        print('Sorry {}, your guess is too high'.format(name))
     else:
-        print('You win!')
+        print('You win, {}! The correct number was {}'.format(name, guess_int))
 
 print('done')
